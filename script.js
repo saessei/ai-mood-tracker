4b3a667c890c39bd4bcff31499c3ff00ee8175dd
Saessei <danielleanne.poral-22@cpu.edu.ph>
2025-09-22 22:56:27 +0800

    Chore: add new assets

---
+++ script.js
@@ -0,0 +1,5 @@
+const messages = document.getElementById("messages");
+const userInput = document.getElementById("userInput");
+const sendBtn = document.getElementById("sendBtn");
+const status = document.getElementById("moodStatus");
+const avatar = document.getElementById("Avatar")
